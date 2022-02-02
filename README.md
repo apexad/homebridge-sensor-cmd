7b032b8b5dc320c553c5e2f6ae7f27536e5dae21
Alex Martin <1437332+apexad@users.noreply.github.com>
2022-02-01 21:23:28 -0700

    now 50 lines, but adds motion sensor and occuoancy sensor

--- README.md
+++ README.md
@@ -5,20 +5,20 @@
 [![npm](https://badgen.net/npm/dt/homebridge-sensor-cmd)](https://www.npmjs.com/package/homebridge-sensor-cmd)
 [![donate](https://badgen.net/badge/donate/paypal/91BE09)](https://www.paypal.me/apexadm)
 
-[Homebridge](https://github.com/homebridge/homebridge) plugin that creates a contact sensor with state determined by a command-line/shell command in only 25 lines of code (and the README is also only 25 lines).
+[Homebridge](https://github.com/homebridge/homebridge) plugin that is a proof of concept to create something functional in a ridiculously small amount of code (50 lines to be exaxt, also this README is just 25 lines).  
+It impliments a Contact/Motion/Occupancy Sensor that is triggered via the output of a command line/shell script.  
+The script simply needs to return a `1` or `0`
 
 ## Configuration
 This easiest way to use this plugin is to use [homebridge-config-ui-x](https://www.npmjs.com/package/homebridge-config-ui-x).  
-To configure manually, add to the `accessories` section of Homebridge's `config.json` after installing the plugin.
+To configure, add to the `accessories` section of Homebridge's `config.json` after installing the plugin.
 
 **Config:**
 ```json
 {
   "accessory": "SensorCmd",
-  "name": "Sensor",
+  "name": "<name of the sensor>",
+  "type": "<sensor type: contact/motion/occupancy) - default is Contact>",
   "command": "<command-line/shell command to execute>"
 }
 ```
-
-## Sponsors
-[mbmccormick](https://github.com/mbmccormick)
--- src/accessory.ts
+++ src/accessory.ts
@@ -1,4 +1,13 @@
-import { AccessoryConfig, AccessoryPlugin, API, CharacteristicEventTypes, CharacteristicGetCallback, HAP, Logging, Service } from 'homebridge';
+import {
+  AccessoryConfig,
+  AccessoryPlugin,
+  API,
+  CharacteristicEventTypes,
+  CharacteristicGetCallback,
+  HAP,
+  Logging,
+  Service,
+} from 'homebridge';
 import { exec } from 'child_process';
 let hap: HAP;
 
@@ -9,16 +18,32 @@ class SensorCmd implements AccessoryPlugin {
   private readonly infoService: Service;
 
   constructor(private readonly log: Logging, private readonly config: AccessoryConfig) {
-    this.log = log;
-    this.sensorService = new hap.Service.ContactSensor(this.config.name);
-    this.sensorService.getCharacteristic(hap.Characteristic.ContactSensorState)
-      .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
-        exec(this.config.command, (err, stdout) => callback(null, err ? 0 : parseInt(stdout)));
-      });
+    switch (this.config.type) {
+      case "motion":
+          this.sensorService = new hap.Service.MotionSensor(this.config.name);
+          this.sensorService.getCharacteristic(hap.Characteristic.MotionDetected)
+          .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+            exec(this.config.command, (err, stdout) => callback(null, err ? 0 : parseInt(stdout)));
+          });
+          break;
+      case "occupancy":
+          this.sensorService = new hap.Service.OccupancySensor(this.config.name);
+          this.sensorService.getCharacteristic(hap.Characteristic.OccupancyDetected)
+          .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+            exec(this.config.command, (err, stdout) => callback(null, err ? 0 : parseInt(stdout)));
+          });
+          break;
+      case "contact":
+      default:
+        this.sensorService = new hap.Service.ContactSensor(this.config.name);
+        this.sensorService.getCharacteristic(hap.Characteristic.ContactSensorState)
+        .on(CharacteristicEventTypes.GET, (callback: CharacteristicGetCallback) => {
+          exec(this.config.command, (err, stdout) => callback(null, err ? 0 : parseInt(stdout)));
+        });
+    }
     this.infoService = new hap.Service.AccessoryInformation()
       .setCharacteristic(hap.Characteristic.Manufacturer, 'apexad')
       .setCharacteristic(hap.Characteristic.Model, 'sensor-cmd');
   }
-
   getServices(): Service[] { return [ this.infoService, this.sensorService ]; }
 }
